ea8569cd2d522a897dccb35e3671919099047d8f
BenWehrend <155917289+BenWehrend@users.noreply.github.com>
2024-01-31 13:04:15 -0700

    bracket and numbers

--- src/warm-up.js
+++ src/warm-up.js
@@ -103,9 +103,11 @@ console.log(books['title3']);
 // Create a new variable "belowTwoHundred" and assign it to an Array of 12
 // different numbers below two hundred.
 
+var belowTwoHundred = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
 
 // Write the code to access the 5th element of the array, using bracket notation.
 
+console.log(belowTwoHundred[4]);
 
 //------------------------- Objects -------------------------//
 
