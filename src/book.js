5235685bbadb3c455506344928e9772030b63a52
BenWehrend <155917289+BenWehrend@users.noreply.github.com>
2024-02-01 22:19:09 -0700

    book final

--- src/book.js
+++ src/book.js
@@ -24,6 +24,20 @@ function calculatePageCount(bookTitle) {
   return bookTitle.length * 20
 };
 
+function writeBook(title, mainCharacter, genre) {
+  var pageCount = calculatePageCount(title);
+  return {
+    title,
+    mainCharacter,
+    pageCount,
+    genre,
+  }
+};
+
+function editBook(newBook) {
+  newBook.pageCount = newBook.pageCount * .75;
+};
+
 module.exports = {
   createTitle,
   buildMainCharacter,
