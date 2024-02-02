744214f85652f3070d7bd9e095292e3a71b8174d
BenWehrend <155917289+BenWehrend@users.noreply.github.com>
2024-02-01 21:56:53 -0700

    Updates

--- src/book.js
+++ src/book.js
@@ -1,12 +1,34 @@
-function createTitle() {
-  
-}
+function createTitle(title) {
+  return `The ${title}`;
+};
+
+
+function buildMainCharacter(name, age, pronouns) { 
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  };
+};
+
+function saveReview(reviewMessage, reviews) {
+  var i;
+  for (i = 0; i < reviews.length; i++) {
+    if (reviews[i] === reviewMessage)
+    return;
+  };
+  reviews.push(reviewMessage);
+};
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20
+};
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
   // writeBook,
   // editBook
 }
