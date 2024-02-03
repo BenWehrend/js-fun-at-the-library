3827b31bd3ebc345ac7e41d51f2936e53d95b66e
BenWehrend <155917289+BenWehrend@users.noreply.github.com>
2024-02-03 14:24:21 -0700

    Update shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,41 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book)
+  }
+};
+
+function unshelfBook(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      shelf.splice(i, 1);
+      return
+    } 
+  } 
+};
+
+function listTitles(shelf) {
+  var bookList = "";
+  for (var i = 0; i < shelf.length; i++) {
+    bookList += shelf[i].title;
+    if (i < shelf.length - 1) {
+      bookList += ", "
+    }
+  } 
+  return bookList
+};
+
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      return true
+    } 
+  } 
+  return false;
+};
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
