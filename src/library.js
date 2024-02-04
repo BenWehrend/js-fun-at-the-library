ffd389f0ff4516c95dc15596a8a1777295754d94
BenWehrend <155917289+BenWehrend@users.noreply.github.com>
2024-02-04 15:22:30 -0700

    Update library.js

--- src/library.js
+++ src/library.js
@@ -14,6 +14,7 @@ function createLibrary(libraryName) {
 // const createLibrary = libraryName => ({name: libraryName, shelves: {fantasy: [], fiction: [], nonFiction: []}});
 
 function addBook(library, book) {
+  // check if book.genre is equal to fantasy, push into fantasy shelf if so. repeated for other genres
   if (book.genre === 'fantasy') {
     library.shelves.fantasy.push(book);
   } else if (book.genre === 'fiction') {
@@ -32,10 +33,15 @@ function addBook(library, book) {
 // };
 
 function checkoutBook(library, title, genre) {
+  // first checks if genre is fantasy
   if (genre === 'fantasy') {
+    // checks the entire length of fantasy shelf array one by one
     for (var i = 0; i < library.shelves.fantasy.length; i++) {
+      // wants to know if the title you're looking if in fantasy shelf array
       if (title === library.shelves.fantasy[i].title) {
+        // removes from fantasy shelf array if so
         library.shelves.fantasy.splice(i, 1);
+        // returns confirmation message
         return `You have now checked out ${title} from the ${library.name}.`
       } 
     } 
@@ -60,13 +66,16 @@ return `Sorry, there are currently no copies of ${title} available at the ${libr
 // bowed out on trying this one. Too complicated for the moment.
 
 function takeStock(library, genre) {
+  // checks if gerne is fantasy
   if (genre === "fantasy") {
+    // returns message containing length of fantasy shelf array from desired library
     return `There are a total of ${library.shelves.fantasy.length} ${genre} books at the ${library.name}.`
   } else if (genre === "ficion") {
     return `There are a total of ${library.shelves.fiction.length} ${genre} books at the ${library.name}.`
   } else if (genre === "nonFiction") {
     return `There are a total of ${library.shelves.nonFiction.length} ${genre} books at the ${library.name}.`
   }
+  // if no genre given returns total of all genres
   return `There are a total of ${library.shelves.fantasy.length + library.shelves.fiction.length + library.shelves.nonFiction.length} books at the ${library.name}.`
 };
 
